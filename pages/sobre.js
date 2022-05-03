d8317f762e502a7ba67855a68c78dfdc620f552c
Jairopedr <62018318+jairopedr@users.noreply.github.com>
2022-05-03 01:03:08 -0300

    Update sobre.js

--- pages/sobre.js
+++ pages/sobre.js
@@ -16,10 +16,10 @@ function Sobre() {
                 <title>SalvamentoRiopas</title>
             </Head>
             <main className="container p-5">
-                <div className="jumbotron jumbotron-fluid">
-                    <h4 className="display-4"> Sobre o Abrigo </h4>  
+                <div className="jumbotron jumbotron-fluid text-center">
+                    <h4 className="text"> Sobre o Abrigo </h4>  
                 </div>
-                <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="row row-cols-1 row-cols-md-2 g-4 p-1">
                     <div class="col">
                         <div className="ratio ratio-16x9"><br></br><br></br>
                             <iframe width="560" height="315" src="https://www.youtube.com/embed/_lVHXSJHDHM?start=2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -40,13 +40,49 @@ function Sobre() {
                         </div>
                     </div> 
                 </div>
-               
-                
+                <hr className="my-4"/>
+                <div id="tips"></div>
+                <div className="container">
+                        <h4 className="display-4">Meios de entrar em contato conosco:</h4>
+                        <h6 className="display-6">Se desejar tirar alguma dúvida estaremos de prontidão em atendê-lo(a) por meio de um dos canais abaixo!</h6>
+                    <div class="row row-cols-1 row-cols-md-3 g-4 m-1">
+                        <div class="col ">
+                            <div className="iconetelefone">
+                                <img src="/telefone.webp" className="email" alt="..." width="90" height="90" />
+                                <p className="lead">(38)998615038</p>
+                                <p className="lead">(38)991367884</p>
+                                <p className="lead">(38)991506021</p>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div className="iconeinstagram">
+                                <img src="/instagram.png" className="email" alt="..." width="90" height="90" />
+                                <p className="lead">@salvamentosriopas</p>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div className="iconemail">
+                                <img src="/email.webp" className="email" alt="..." width="80" height="80" /><br></br>
+                                <p>salvamentosriopas1@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                    
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
                 <script src="custom.js"></script>
             </main>
         </>
     )
 }
+
+export async function getServerSideProps(){
+    const response = await fetch(`http://localhost:8080/`);
+    const data= await response.json();
+    // console.log(data);//conferir se há os dados
+
+    return { props: { data } };
+}
+
 
 export default Sobre;
